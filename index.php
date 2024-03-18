f8a6bfa42774e2fa145f6225343658c272dc1d4f
Nguyễn Thị Kim Nga <138105959+kng141@users.noreply.github.com>
2024-03-18 17:16:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="puplic/css/style.css">
+
+</head>
+<body>
+    <div class="container">
+
+        <div class="menutong">
+             <div id="menu">
+                 <div class="logo">
+                    <img src="puplic/file/logo.png" alt="" >
+                 </div> 
+                 <div class="function">
+                    <a href="" id="home">
+                        <img src="puplic/file/home.png" alt="">
+                        <p>Home</p>
+                    </a>
+                    <a href="" id="search">
+                        <img src="puplic/file/search.png" alt="">
+                        <p>Search</p>
+                    </a>
+                    <a href="">
+                        <img src="puplic/file/list.png" alt="">
+                        <p>Your Library</p>
+                    </a>
+
+                    <div id="chucnang">
+                        <a href="" id="user">
+                            <img src="puplic/file/create.png" alt="">
+                            <p>Create Playlist</p>
+                        </a>
+                        <a href="" id="user">
+                            <img src="puplic/file/likesong.png" alt="">
+                            <p>Liked Song</p>
+                        </a>
+                        <a href="" id="user">
+                            <img src="puplic/file/your.png" alt="">
+                            <p>Your Episodes</p>
+                        </a>
+                    </div>
+                 </div>
+             </div>  
+             
+             <div id="menu2">
+                <p style=" color: rgb(212, 211, 211);
+                font-size: large;
+                margin-left: 25px; line-height: 60px;">Playlist Name</p>
+             </div>
+             </div>
+
+        <div id="right">
+            <div class="toolbar">
+
+                <div style="display: flex;">
+                    <img src="puplic/file/lon - Copy.png" alt="" style="width: 25px; height: 25px; margin-right: 25px; margin-top: 21px; margin-left: 55px;">
+                    <img src="puplic/file/lon.png" alt="" style="width: 25px; height: 25px; margin-top: 20px; margin-right: 50px;">
+                    <!-- <input type="search" name="" id="" value="Tìm kiếm bài hat" style="width: 350px; height: 40px; margin-top: 10px; border-radius: 10px;
+                     background-color: #413545;"> 
+                    <a href="" style="text-decoration: none; font-size: larger; color: white; font-weight: 600; margin-left: 50px;">
+                        <p>Playlist</p>
+                    </a>  -->
+                    <a href="login.php" id="login">
+                        <p>Login</p>
+                    </a>
+                    <a href="" id="sign">
+                        <p>Sign Up </p>
+                    </a>
+                    
+                    
+                </div>
+                
+            </div>
+            <div id="content">
+                <h2 style="color: white; margin-bottom: 35px;">Popular Song</h2>
+                <div id="listbaihat" style="display: flex;">
+                    <main id="bai1">
+                        <img src="puplic/file/khongthesay.jpg" alt="">
+                        <div>
+                            <b>Không Thể Say</b>
+                            <p>HIEUTHUHAI</p>
+                        </div>
+                    </main>
+
+                    <main id="bai1">
+                        <img src="puplic/file/chiucachminhnoithua.jpg" alt="">
+                        <div>
+                            <b>Chịu Cách Mình N..</b>
+                            <p>Rhydel, Coolkid</p>
+                        </div>
+                    </main>
+
+                    <main id="bai1">
+                        <img src="puplic/file/sauloitukhuocc.jpg" alt="">
+                        <div>
+                            <b>Sau Lời Từ Khước</b>
+                            <p>Phan Mạnh Quỳnh</p>
+                        </div>
+                    </main>
+
+                </div>
+                <div id="list2" style="display: flex; margin-top: 30px;">
+                    <main id="bai1">
+                        <img src="puplic/file/thienlyoi (2).jpg" alt="">
+                        <div>
+                            <b>Thiên Lý Ơi</b>
+                            <p>Jack</p>
+                        </div>
+                    </main>
+
+                    <main id="bai1">
+                        <img src="puplic/file/buonhayvui.jpg" alt="">
+                        <div>
+                            <b>Buồn Hay Vui</b>
+                            <p>MCK</p>
+                        </div>
+                    </main>
+
+                    <main id="bai1">
+                        <img src="puplic/file/saophaikhoc.jpg" alt="">
+                        <div>
+                            <b>Dân Chơi Sao..</b>
+                            <p>Andree Right Hand</p>
+                        </div>
+                    </main>
+                </div>
+
+                <h2 style="color: white; margin-bottom: 0px; margin-top: 50px;">Album Hot</h2>
+                <div id="albumhot" style="display: flex;">
+                    <main id="album" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/100nhactre.jpg" alt="">
+                        <h2>Top 100 Nhạc Trẻ</h2>
+                    </main>
+                    <main id="album" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/100vpop.jpg" alt="">
+                        <h2>Top 100 V-Pop</h2>
+                    </main>
+                    <main id="album" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/100edm.jpg" alt="">
+                        <h2>Top 100 EDM VIỆT</h2>
+                    </main>
+                    <main id="album" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/nhactiktok.jpg" alt="">
+                        <h2>TikTok MUSIC</h2>
+                    </main>
+                </div>
+
+                <h2 style="color: white; margin-bottom: 35px; margin-top: 50px;">Popular Artists</h2>
+                <div id="nghesi" style="display: flex;">
+                    <main id="namenghesi">
+                        <img src="puplic/file/sontung.webp" alt="">
+                        <h3>SƠN TÙNG - MTP</h3>
+                    </main>
+                    <main id="namenghesi">
+                        <img src="puplic/file/phanmanhquynh.jpg" alt="">
+                        <h3>PHAN MẠNH QUỲNH</h3>
+                    </main>
+                </div>
+                <div id="nghesi" style="display: flex; margin-top: 30px;">
+                    <main id="namenghesi">
+                        <img src="puplic/file/rhyder.webp" alt="">
+                        <h3>RHYDER</h3>
+                    </main>
+                    <main id="namenghesi">
+                        <img src="puplic/file/mck.jpg" alt="">
+                        <h3>MCK</h3>
+                    </main>
+                </div>
+                <div id="nghesi" style="display: flex; margin-top: 30px;">
+                    <main id="namenghesi">
+                        <img src="puplic/file/hieuthuhai.jpg" alt="">
+                        <h3>HIEU THU HAI</h3>
+                    </main>
+                    <main id="namenghesi">
+                        <img src="puplic/file/denvau.jpg" alt="">
+                        <h3>ĐÊN VÂU</h3>
+                    </main>
+                </div>
+
+                <!-- <h2 style="color: white; margin-bottom: 5px; margin-top: 50px;">Album Hot</h2>
+                <div id="albumhot" style="display: flex;">
+                    <main id="album" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/100nhactre.jpg" alt="">
+                        <h2>Top 100 Nhạc Trẻ</h2>
+                    </main>
+                    <main id="album" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/100vpop.jpg" alt="">
+                        <h2>Top 100 V-Pop</h2>
+                    </main>
+                    <main id="album" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/100edm.jpg" alt="">
+                        <h2>Top 100 EDM VIỆT</h2>
+                    </main>
+                    <main id="album" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/nhactiktok.jpg" alt="">
+                        <h2>TikTok MUSIC</h2>
+                    </main>
+                </div> -->
+
+              <div>
+                <h2 style="color: white; margin-bottom: 0px; margin-top: 50px;">Suggestions for you</h2>
+                <div id="foryou" style="display: flex;">
+                    <main id="you" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/cothemuonghe1.jpg" alt="">
+                        <p>Remix Hot</p>
+                    </main>
+                    <main id="you" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/cothemuonghe2.jpg" alt="">
+                        <p>Thêm Một Ai Cũng Dư Thừa</p>
+                    </main>
+                    <main id="you" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/cothemuonghe3.jpg" alt="">
+                        <p>người thứ ba</p>
+                    </main>
+                    <main id="you" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/cothemuonghe4.jpg" alt="">
+                        <p>Âm Thầm Bên Em</p>
+                    </main>
+                    <main id="you" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/cothemuonghe5.jpg" alt="">
+                        <p>Acoustic </p>
+                    </main>
+                    <main id="you" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/cothemuonghe6.jpg" alt="">
+                        <p>V-Pop</p>
+                    </main>
+                </div>
+              </div>
+
+              <div>
+                <h2 style="color: white; margin-bottom: 0px; margin-top: 80px;">Maybe you want to listen</h2>
+                <div id="foryou" style="display: flex;">
+                    <main id="you" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/thuytrieu.jpg" alt="">
+                        <p>Thủy Triều</p>
+                    </main>
+                    <main id="you" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/sauloitukhuocc.jpg" alt="">
+                        <p>Sau Lời Từ Khước</p>
+                    </main>
+                    <main id="you" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/nangchentieusau.jpg" alt="">
+                        <p>Nâng Chén Tiêu Sầu</p>
+                    </main>
+                    <main id="you" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/catdoinoisau.jpg" alt="">
+                        <p>Cắt Đôi Nỗi Sầu</p>
+                    </main>
+                    <main id="you" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/thienlyoi (2).jpg" alt="">
+                        <p>Thiên Lý Ơi</p>
+                    </main>
+                    <main id="you" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/hoadiphucdenn.jpg" alt="">
+                        <p>Họa Đi Phúc Đến</p>
+                    </main>
+                </div>
+              </div>
+
+              <div>
+                <h2 style="color: white; margin-bottom: 0px; margin-top: 80px;">Chill Viet </h2>
+                <div id="chilldi" style="display: flex;">
+                    <main id="chill" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/chill6.jpg" alt="">
+                        <p>Ngốc à, mình chỉ yêu mỗi mình cậu thôi</p>
+                    </main>
+                    <main id="chill" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/chill5.jpg" alt="">
+                        <p>Nhạc việt "Lâu phai" và gây nghiện hoài hoài</p>
+                    </main>
+                    <main id="chill" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/chill4.jpg" alt="">
+                        <p>Nâng niu đôi tài cũng R&B Việt</p>
+                    </main>
+                    <main id="chill" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/chill3.jpg" alt="">
+                        <p>Một list nhạc mang làm sự bình yên cho bạn </p>
+                    </main>
+                    <main id="chill" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/chill2.jpg" alt="">
+                        <p>Đắm say với nổi buồn qua những giai điệu cực chill</p>
+                    </main>
+                    <main id="chill" style="display: flex; flex-direction: column; margin-top: 30px;">
+                        <img src="puplic/file/chill1.jpg" alt="">
+                        <p>Ngốc à, mình chỉ yêu mỗi mình cậu thôi</p>
+                    </main>
+                </div>
+              </div>
+              <footer>
+                <div id="one">
+                  <h2>Companny</h2>
+                  <a href="">About</a><br />
+                  <br>
+                  <a href="">Jobs</a><br />
+                  <br>
+                  <a href="">For The Record</a><br />
+                </div>
+                <div id="two">
+                  <h2>Communnities</h2>
+                  <a href="">For Artists</a><br /><br>
+                  <a href="">Developers</a><br /><br>
+                  <a href="">Advertising</a><br /><br>
+                  <a href="">Investore</a><br />
+                </div>
+                <div id="three">
+                  <h2>Useful links</h2>
+                  <a href="">Support</a><br /><br>
+                  <a href="">Free Mobile App</a><br />
+                </div>
+                <div id="four">
+                  <a href=""><img src="puplic/file/fb.png" alt="" /></a>
+                  <a href=""><img src="puplic/file/ig.png" alt="" /></a>
+                  <a href=""><img src="puplic/file/tw.png" alt="" /></a>
+                </div>
+              </footer>
+
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
+
+<!-- <audio controls style="width: 500px;">
+    <source src="file/SauLoiTuKhuocThemeSongFromMAI-PhanManhQuynh-13780092.mp3">
+</audio> -->
+
+<!-- <div class="search-box">
+    <input type="text" placeholder="Tìm kiếm...">
+    <button type="submit"><i class="fas fa-search"></i></button>
+  </div> -->
